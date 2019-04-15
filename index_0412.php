5ab08bf5cb4636f253280d2dce885f992dbb7c76
matt <kerosu1991@gmail.com>
2019-04-15 20:49:42 +0800

    footer

---
+++ index_0412.php
@@ -0,0 +1,203 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/main.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+    <style>
+    .parallax {
+  /* The image used */
+  background-image: url("img/best-italian-espresso-machine1.jpg");
+
+  /* Set a specific height */
+  min-height: 718px; 
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+    .upbackground{
+            width:98%;
+            height:auto;
+            margin-left: 1%;
+            margin-right: 1%;
+            margin-top: 58px;
+            opacity: 0.9;
+  /* position: fixed;  */
+    }
+    .navbar{
+            position:fixed;
+            z-index:2;
+            width:100%;
+            /* padding:上 右 下 左; */
+            padding:5px 0px 5px 5px;
+            /* padding-top:5px;
+            padding-right:0px;
+            padding-bottom:5px;
+            padding-left:5px;
+           */
+        
+    }
+
+    /* 回到最上方css */
+ 
+    /* 回到最上方css結束 */
+    </style>
+  </head>
+  <body>
+
+
+<!-- preloader -->
+          <div class="preloader">
+    <img src="img/preloader.gif" alt="preloader" class="preloader__item">
+  </div>
+
+<script>
+window.addEventListener("load",function(){
+
+    document.querySelector(".preloader").style.display="none";
+})
+
+</script>
+
+  <!-- end preloader -->
+      <!-- nav 最上方欄位-->
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">Navbar</a>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+      <!-- nav end  最上方選擇欄結束-->
+    <!-- 背景圖-->
+    <div class="parallax" style="" >
+
+    <!-- <img class="upbackground" src="img/best-italian-espresso-machine1.jpg" alt="sroll img" title=""> -->
+
+
+    </div>
+    <!-- 暫時文字開始 -->
+ <div style="color:#008080;background-color:white ">   
+<p>
+今天，就是全人類見證宇宙黑洞的一刻！事件視界望遠鏡計畫於今（10）日召開全球連線記者會，發布人類史上第一張「黑洞剪影」照片，與地球一樣大的虛擬洲際望遠鏡拍下了位於 M87 星系中心的超大質量黑洞。你必須知道這次的成就有兩大重要意義：第一，科學家首次以「直接證據」證明黑洞存在；第二，愛因斯坦繼續屹立不搖。
+
+
+
+
+黑洞是個全暗物體，基本上融入在同樣全黑的宇宙背景中，我們無法直接看見黑洞在哪兒，加上黑洞一旦形成就會開始吸取附近所有物質、光線、電磁波，因此尋找黑洞是一件相當困難的工作。
+
+過去，天文學家尋找黑洞都是透過間接證據，比如發現圍繞著黑洞的吸積盤、黑洞噴出的相對論性噴流、重力波、或恆星運動等去推測，儘管這些間接證據都很有力，但不是直接拍下黑洞，科學家心底總是會對未知事物存疑。
+
+而事件視界望遠鏡（EHT）計畫要做的工作，就是將分布在地球各地的電波望遠鏡以特長基線干涉技術連接起來，變成一個和地球一樣大的巨型虛擬望遠鏡，使儀器的角解析度精確到足以直接觀測黑洞事件視界（Event Horizon），也就是黑洞的邊界。
+
+在美國華盛頓、台灣中研院、聖地牙哥、布魯塞爾、日本東京、中國上場等全球 6 場連線記者會中，天文學家們終於發布了第一張黑洞事件視界影像，是位於距離地球 5,500 萬光年、M87 星系中心質量達地球 65 億倍的超大質量黑洞。
+
+在此次觀測計畫中所使用的 8 座電波望遠鏡，有 3 座是由台灣中研院支援運轉（SMA、ALMA、JCMT），表明台灣對此次研究的重大貢獻，這次成果發表在科學上的重要性有三點，第一：揭開活躍星系中心的超大質量黑洞神秘面紗；第二，探索極限條件下重力理論的新工具；第三，成為研究黑洞相關天文物理（比如電漿物理）的先鋒。
+
+（後續將持續更新）
+
+（首圖來源：中研院提供）
+</p>
+<p>
+今天，就是全人類見證宇宙黑洞的一刻！事件視界望遠鏡計畫於今（10）日召開全球連線記者會，發布人類史上第一張「黑洞剪影」照片，與地球一樣大的虛擬洲際望遠鏡拍下了位於 M87 星系中心的超大質量黑洞。你必須知道這次的成就有兩大重要意義：第一，科學家首次以「直接證據」證明黑洞存在；第二，愛因斯坦繼續屹立不搖。
+
+
+
+
+黑洞是個全暗物體，基本上融入在同樣全黑的宇宙背景中，我們無法直接看見黑洞在哪兒，加上黑洞一旦形成就會開始吸取附近所有物質、光線、電磁波，因此尋找黑洞是一件相當困難的工作。
+
+過去，天文學家尋找黑洞都是透過間接證據，比如發現圍繞著黑洞的吸積盤、黑洞噴出的相對論性噴流、重力波、或恆星運動等去推測，儘管這些間接證據都很有力，但不是直接拍下黑洞，科學家心底總是會對未知事物存疑。
+
+而事件視界望遠鏡（EHT）計畫要做的工作，就是將分布在地球各地的電波望遠鏡以特長基線干涉技術連接起來，變成一個和地球一樣大的巨型虛擬望遠鏡，使儀器的角解析度精確到足以直接觀測黑洞事件視界（Event Horizon），也就是黑洞的邊界。
+
+在美國華盛頓、台灣中研院、聖地牙哥、布魯塞爾、日本東京、中國上場等全球 6 場連線記者會中，天文學家們終於發布了第一張黑洞事件視界影像，是位於距離地球 5,500 萬光年、M87 星系中心質量達地球 65 億倍的超大質量黑洞。
+
+在此次觀測計畫中所使用的 8 座電波望遠鏡，有 3 座是由台灣中研院支援運轉（SMA、ALMA、JCMT），表明台灣對此次研究的重大貢獻，這次成果發表在科學上的重要性有三點，第一：揭開活躍星系中心的超大質量黑洞神秘面紗；第二，探索極限條件下重力理論的新工具；第三，成為研究黑洞相關天文物理（比如電漿物理）的先鋒。
+
+（後續將持續更新）
+
+（首圖來源：中研院提供）
+</p>
+<p>
+今天，就是全人類見證宇宙黑洞的一刻！事件視界望遠鏡計畫於今（10）日召開全球連線記者會，發布人類史上第一張「黑洞剪影」照片，與地球一樣大的虛擬洲際望遠鏡拍下了位於 M87 星系中心的超大質量黑洞。你必須知道這次的成就有兩大重要意義：第一，科學家首次以「直接證據」證明黑洞存在；第二，愛因斯坦繼續屹立不搖。
+
+
+
+
+黑洞是個全暗物體，基本上融入在同樣全黑的宇宙背景中，我們無法直接看見黑洞在哪兒，加上黑洞一旦形成就會開始吸取附近所有物質、光線、電磁波，因此尋找黑洞是一件相當困難的工作。
+
+過去，天文學家尋找黑洞都是透過間接證據，比如發現圍繞著黑洞的吸積盤、黑洞噴出的相對論性噴流、重力波、或恆星運動等去推測，儘管這些間接證據都很有力，但不是直接拍下黑洞，科學家心底總是會對未知事物存疑。
+
+而事件視界望遠鏡（EHT）計畫要做的工作，就是將分布在地球各地的電波望遠鏡以特長基線干涉技術連接起來，變成一個和地球一樣大的巨型虛擬望遠鏡，使儀器的角解析度精確到足以直接觀測黑洞事件視界（Event Horizon），也就是黑洞的邊界。
+
+在美國華盛頓、台灣中研院、聖地牙哥、布魯塞爾、日本東京、中國上場等全球 6 場連線記者會中，天文學家們終於發布了第一張黑洞事件視界影像，是位於距離地球 5,500 萬光年、M87 星系中心質量達地球 65 億倍的超大質量黑洞。
+
+在此次觀測計畫中所使用的 8 座電波望遠鏡，有 3 座是由台灣中研院支援運轉（SMA、ALMA、JCMT），表明台灣對此次研究的重大貢獻，這次成果發表在科學上的重要性有三點，第一：揭開活躍星系中心的超大質量黑洞神秘面紗；第二，探索極限條件下重力理論的新工具；第三，成為研究黑洞相關天文物理（比如電漿物理）的先鋒。
+
+（後續將持續更新）
+
+（首圖來源：中研院提供）
+</p>
+<p>
+今天，就是全人類見證宇宙黑洞的一刻！事件視界望遠鏡計畫於今（10）日召開全球連線記者會，發布人類史上第一張「黑洞剪影」照片，與地球一樣大的虛擬洲際望遠鏡拍下了位於 M87 星系中心的超大質量黑洞。你必須知道這次的成就有兩大重要意義：第一，科學家首次以「直接證據」證明黑洞存在；第二，愛因斯坦繼續屹立不搖。
+
+
+
+
+黑洞是個全暗物體，基本上融入在同樣全黑的宇宙背景中，我們無法直接看見黑洞在哪兒，加上黑洞一旦形成就會開始吸取附近所有物質、光線、電磁波，因此尋找黑洞是一件相當困難的工作。
+
+過去，天文學家尋找黑洞都是透過間接證據，比如發現圍繞著黑洞的吸積盤、黑洞噴出的相對論性噴流、重力波、或恆星運動等去推測，儘管這些間接證據都很有力，但不是直接拍下黑洞，科學家心底總是會對未知事物存疑。
+
+而事件視界望遠鏡（EHT）計畫要做的工作，就是將分布在地球各地的電波望遠鏡以特長基線干涉技術連接起來，變成一個和地球一樣大的巨型虛擬望遠鏡，使儀器的角解析度精確到足以直接觀測黑洞事件視界（Event Horizon），也就是黑洞的邊界。
+
+在美國華盛頓、台灣中研院、聖地牙哥、布魯塞爾、日本東京、中國上場等全球 6 場連線記者會中，天文學家們終於發布了第一張黑洞事件視界影像，是位於距離地球 5,500 萬光年、M87 星系中心質量達地球 65 億倍的超大質量黑洞。
+
+在此次觀測計畫中所使用的 8 座電波望遠鏡，有 3 座是由台灣中研院支援運轉（SMA、ALMA、JCMT），表明台灣對此次研究的重大貢獻，這次成果發表在科學上的重要性有三點，第一：揭開活躍星系中心的超大質量黑洞神秘面紗；第二，探索極限條件下重力理論的新工具；第三，成為研究黑洞相關天文物理（比如電漿物理）的先鋒。
+
+（後續將持續更新）
+
+（首圖來源：中研院提供）
+</p>
+</div>
+<!--回到最上方按鈕 -->
+<button class="toTop-arrow" id="BackTop" ></button> 
+<!--回到最上方按鈕結束-->
+<script>
+    // 回到最上方代碼
+
+</script>
+<!-- 暫時文字結束 -->
+   
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!-- <script src="js/app.js"></script> -->
+</body>
+</html>
